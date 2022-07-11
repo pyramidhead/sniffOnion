c4d54c528a683514b4bf4c086a93fd661275f986
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-07-11 14:12:39 -0700

    Update sniffOnion.sh
    fixing stupidity

--- sniffOnion.sh
+++ sniffOnion.sh
@@ -2,7 +2,6 @@
 # this script hides a host behind a tor exit node
 
 # clone git repo into correct directory
-mkdir ~/git/torghost
-cd ~/git/torghost
+cd ~/git
 git clone https://github.com/susmithHCK/torghost.git
 cd ~/
