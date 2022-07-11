ac16b9fe5d3c1d08df3a248e966ec42ddd9ddd06
Pyramid Ronin <pyramidhead@users.noreply.github.com>
2022-07-11 14:51:57 -0700

    Update sniffOnion.sh
    prereqs

--- sniffOnion.sh
+++ sniffOnion.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # this script hides a host behind a tor exit node
 
+# ensure prerequisites
+sudo apt-get install python3-dev
+
 # clone git repo into correct directory
 git clone https://github.com/susmithHCK/torghost.git ~/git/torghost
 sudo chmod 755 ~/git/torghost/build.sh
